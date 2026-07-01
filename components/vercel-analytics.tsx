49c50eb692d062a7e28a8d2f4b08c8dc0dbfbb03
Benjamin Sakarias Jansson Pedersen <benped2006@icloud.com>
2026-07-01 14:20:58 +0200

    added speed insight

--- components/vercel-analytics.tsx
+++ components/vercel-analytics.tsx
@@ -1,7 +1,11 @@
-"use client";
-
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export function VercelAnalytics() {
-  return <Analytics />;
+export function VercelInsights() {
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
