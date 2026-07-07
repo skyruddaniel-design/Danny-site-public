4bd007099121cde57117fab865e84fac76c7521b
skyruddaniel-design <skyruddaniel@gmail.com>
2026-07-07 10:06:55 +0200

    yes 12

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return children;
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
